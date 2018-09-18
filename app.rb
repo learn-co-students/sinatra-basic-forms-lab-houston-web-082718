d16276ba26501005bfb3cb5de850f1fdca29a4cf
github username <github email address>
2018-09-18 15:39:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
         erb :create_puppy
     end
 
-    post '/pup' do
+    post '/' do
         @name = params[:name]
         @breed = params[:breed]
         @age = params[:age]
